b42e08d518c9909df5494eec33f84cdfcf6a2139
sherin <you@example.com>
2020-07-14 17:44:18 +0530

    conatct us page

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,7 +4,89 @@ import './style.css';
 export default class Footer extends Component {
   render() {
     return (
-      <div> Footer</div>
+   
+<footer className="page-footer font-small blue pt-4">
+
+  
+  <div className="container-fluid text-center text-md-left">
+
+    
+    <div className="row">
+
+     
+      <div className="col-md-6 mt-md-0 mt-3">
+
+       
+        <h5 className="text-uppercase">Footer Content</h5>
+        <p>Here you can use rows and columns to organize your footer content.</p>
+
+      </div>
+      
+
+      
+
+     
+      <div className="col-md-3 mb-md-0 mb-3">
+
+       
+        <h5 className="text-uppercase">Links</h5>
+
+        <ul className="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+    
+
+      
+      <div class="col-md-3 mb-md-0 mb-3">
+
+       
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+     
+
+    </div>
+  
+
+  </div>
+
+
+ 
+  <div className="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="#"> sample.com</a>
+  </div>
+
+
+</footer>
+
     )
   }
 }
--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -1,10 +1,115 @@
 import React, { Component } from 'react';
+import { NavLink,Link } from "react-router-dom";
 import './style.css';
+import Header from '../../components/header/index.jsx';
+import Footer from '../../components/footer/index.jsx';
 
-export default class Contact extends Component {
+export default class Home extends Component {
+
+
+
+	 constructor() {
+   super();
+this.state = {
+      fields: {
+        name: '',
+        email: '',
+       
+        phone: ''
+      },
+      errors: {},
+   }
+   
+   this.handleChange = this.handleChange.bind(this);
+   this.onSubmit = this.onSubmit.bind(this);
+   
+   
+  };
+  handleChange(e) {
+    const target = e.target;
+    let fields = this.state.fields;
+    fields[e.target.name] = e.target.value;
+    this.setState({
+      fields
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    if (this.validateForm()) {
+      console.log('Form data:')
+      console.log(this.state.fields)
+      this.props.history.push('/home');
+    }
+  }
+
+  validateForm() {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+    
+
+    if (!fields["name"]) {
+      formIsValid = false;
+      errors["name"] = "*Please enter your name.";
+    }
+
+    if (!fields["email"]) {
+      formIsValid = false;
+      errors["email"] = "*Please enter your email.";
+    }
+
+    
+
+
+    if (!fields["phone"]) {
+      formIsValid = false;
+      errors["phone"] = "*Please enter your phone.";
+    }
+
+    this.setState({
+      errors: errors
+    });
+    return formIsValid;
+  }
+
+
+
+
   render() {
     return (
-      <div> Contact</div>
+    	<div class="home-main">
+    	  <Header></Header>
+      <div className="form-blk">
+                          <form onSubmit={this.onSubmit}>
+
+
+                          
+                             <div className="form-row">
+                              <label>Full name:</label>
+                              <input type="text" name="name" className="input-style" value={this.state.fields.name} onChange={this.handleChange} />
+                              <div className="errorMsg">{this.state.errors.name}</div>
+                            </div>
+                            <div className="form-row">
+                              <label>Email:</label>
+                              <input type="email" name="email" className="input-style" value={this.state.fields.email} onChange={this.handleChange} />
+                              <div className="errorMsg">{this.state.errors.email}</div>
+                            </div>
+                            <div className="form-row">
+                              <label>Phone no:</label>
+                              <input type="tel" name="phone" className="input-style" value={this.state.fields.phone} onChange={this.handleChange} />
+                              <div className="errorMsg">{this.state.errors.phone}</div>
+                            </div>
+                            
+                             
+                           
+                            <div className="form-row btn-blk two-btns-blk">
+                              <button className="btn-common" type="submit">Submit</button>
+                            </div>
+                          </form>
+                        </div>
+        <Footer></Footer>
+        </div>
     )
   }
 }
